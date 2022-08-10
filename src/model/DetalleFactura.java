ccc4891c73c0410c1181ce38c1895478238464db
Juan Esteban Parra Parra <juane.parrap@uqvirtual.edu.co>
2022-08-10 00:47:15 -0500

    Initial commit

---
+++ src/model/DetalleFactura.java
@@ -0,0 +1,33 @@
+package model;
+
+public class DetalleFactura {
+
+    private Producto producto;
+    private int cantidad;
+    private double total;
+
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+}
---
+++ src/model/Juridica.java
@@ -0,0 +1,57 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Juridica {
+
+
+    private String nombre;
+    private String nit;
+
+    //clases
+
+    private ArrayList<Factura> listaFacturas;
+    private ArrayList<Producto> listaProductos;
+    private ArrayList<Cliente> listaClientes;
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNit() {
+        return nit;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
+
+    public ArrayList<Factura> getListaFacturas() {
+        return listaFacturas;
+    }
+
+    public void setListaFacturas(ArrayList<Factura> listaFacturas) {
+        this.listaFacturas = listaFacturas;
+    }
+
+    public ArrayList<Producto> getListaProductos() {
+        return listaProductos;
+    }
+
+    public void setListaProductos(ArrayList<Producto> listaProductos) {
+        this.listaProductos = listaProductos;
+    }
+
+    public ArrayList<Cliente> getListaClientes() {
+        return listaClientes;
+    }
+
+    public void setListaClientes(ArrayList<Cliente> listaClientes) {
+        this.listaClientes = listaClientes;
+    }
+}
